bbc5a9db75482738d412ecc20e982d7001800540
LeoCR <leonardoas2007@gmail.com>
2019-08-05 10:53:53 -0600

    Updated private.route.js

--- private/app/route/private.route.js
+++ private/app/route/private.route.js
@@ -4,13 +4,13 @@ module.exports = function(app,express,path,isLoggedIn) {
         res.status(200).sendFile(path.resolve(__dirname+'/../../../../react-redux-checkout-restaurant/build/index.html'));
     })
     app.get([
-    '/admin/edit/user/:id','/admin/users',
+    '/admin/users/:page','/admin/edit/user/:id','/admin/users','/admin/add/user',
     '/admin/main-courses','/admin/add/main-course','/admin/main-courses/:page',
     '/admin/desserts','/admin/add/dessert','/admin/desserts/:page',
     '/admin/appetizers','/admin/add/appetizer','/admin/appetizers/:page',
     '/admin/ingredients','/admin/add/ingredient','/admin/ingredients/:page',
     '/admin/drinks','/admin/add/drink','/admin/drinks/:page',
-    '/admin/invoices','/admin/edit/invoice/:order_code'
+    '/admin/invoices','/admin/edit/invoice/:order_code','/admin/invoices/:page'
     ],isLoggedIn,function(req,res){
         res.status(200).sendFile(path.resolve(__dirname+'/../../../../react-admin-restaurant/build/index.html'));
     })
